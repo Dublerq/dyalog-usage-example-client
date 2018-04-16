d9ae5945611efe9b4fa8df0b1c94ed850513cb91
Dublerq <lromanowicz1@gmail.com>
2018-04-16 21:43:53 +0200

    Additional Dyalog setup example

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,19 @@
 #include "../lib/dyalog/Model/Messages/CriticalMessage.h"
 
 int main() {
+    //init dyalog lib
     std::shared_ptr<Dyalog> logger(new Dyalog());
 
+    //setup dyalog configs
+    logger->getConfig()->setCodeTraceLogged(true);
+
+    //add handlers
     std::shared_ptr<LogHandlerAbstract> writer(new SimpleLogWriter("test.txt"));
     logger->registerLogHandler(writer);
 
+    //log a custom type message
     std::shared_ptr<CriticalMessage> message(new CriticalMessage());
     message->setMessage("test");
-
     logger->log(message);
 
     return 0;
