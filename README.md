fc1f2930ae93c0ae5d5e281ade5b4a5a3bcd9e68
Dublerq <lromanowicz1@gmail.com>
2018-04-19 21:10:06 +0200

    Sending logs through TCP

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dyalog-usage-example-client
 Example application simulating a work of real program (web backend in this case) written in C++ utilizing Dyalog library features. Part of a university project.
+
+### How to test remote tcp connection
+Client/Server implementation depends on the end user requirements. Because of that it has not been written.
+Client example is implemented as `SimpleTCPLogSender`. To simulate server netcat might be used. 
+Example command would be: `nc -k -l localhost 6000 >> /tmp/log.txt` to listen continously on `127.0.0.1:6000` and save results to `/tmp/log.txt`.
+To browse logs use your favorite text viewer/editor.
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #include "../lib/dyalog/Model/Messages/CriticalMessage.h"
 #include "../lib/dyalog/Model/Messages/DebugMessage.h"
 #include "../lib/dyalog/Model/Messages/NoticeMessage.h"
+#include "../lib/dyalog/Service/LogSenders/SimpleTCPLogSender.h"
+
+#define TARGET_SERVER_IP "127.0.0.1"
+#define TARGET_SERVER_PORT 6000
 
 void testSyncWriting(){
     //init dyalog lib
@@ -15,13 +19,30 @@ void testSyncWriting(){
     //setup dyalog configs
     logger->getConfig()->setCodeTraceLogged(true);
 
-    //add handlers
+    //add handler
     std::shared_ptr<LogHandlerAbstract> writer(new SimpleLogWriter("test.txt"));
     logger->registerLogHandler(writer);
 
     //log a custom type message
     std::shared_ptr<DebugMessage> message(new DebugMessage());
     message->setMessage("sync test");
+    logger->log(message);
+}
+
+void testTCPSending(){
+    //init dyalog lib
+    std::shared_ptr<Dyalog> logger(new Dyalog());
+
+    //setup dyalog configs
+    logger->getConfig()->setCodeTraceLogged(true);
+
+    //add handler
+    std::shared_ptr<LogHandlerAbstract> sender(new SimpleTCPLogSender(TARGET_SERVER_IP, TARGET_SERVER_PORT));
+    logger->registerLogHandler(sender);
+
+    //log a custom type message
+    std::shared_ptr<DebugMessage> message(new DebugMessage());
+    message->setMessage("Sending log through tcp!");
     logger->log(message);
 }
 
@@ -32,8 +53,7 @@ void testAsyncWriting(){
     //setup dyalog configs
     logger->getConfig()->setCodeTraceLogged(true);
 
-    //add handlers
-    //std::shared_ptr<LogHandlerAbstract> writer(new SimpleLogWriter("test.txt"));
+    //add handler
     std::shared_ptr<LogHandlerAbstract> writer(new AsyncSimpleLogWriter("test.txt"));
     logger->registerLogHandler(writer);
 
@@ -55,6 +75,7 @@ void testAsyncWriting(){
 }
 
 int main() {
+    testTCPSending();
     testSyncWriting();
     testAsyncWriting();
 
